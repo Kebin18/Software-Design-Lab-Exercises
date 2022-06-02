2837a788fcf66f51749a374fbb0404fb56ca188c
Mark Kevin B. Bangloy <101876367+Kebin18@users.noreply.github.com>
2022-06-02 20:09:02 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2C_BANGLOY, MARK KEVIN B._LAB REPORT#3/single (creation).py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data):
+        self.data = data;
+        self.next = None;
+
+
+class CountNodes:
+
+    def __init__(self):
+        self.head = None;
+        self.tail = None;
+
+    def addNode(self, data):
+        newNode = Node(data);
+
+        if (self.head == None):
+            self.head = newNode;
+            self.tail = newNode;
+
+        else:
+            self.tail.next = newNode;
+            self.tail = newNode;
+
+    def countNodes(self):
+        count = 0;
+        current = self.head;
+        while (current != None):
+            count = count + 1;
+            current = current.next;
+
+        return count;
+
+    def display(self):
+        current = self.head;
+        if (self.head == None):
+            print("List is empty");
+            return;
+        print("Nodes of singly linked list: ");
+        while (current != None):
+            print(current.data),
+            current = current.next;
+
+
+sList = CountNodes();
+
+sList.addNode(10);
+sList.addNode(20);
+sList.addNode(30);
+sList.addNode(40);
+
+sList.display();
+
+print("Count of nodes present in the list: " + str(sList.countNodes()));  
