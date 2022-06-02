3ec98bbd1dc544334763f615e98f2174f75fc711
Mark Kevin B. Bangloy <101876367+Kebin18@users.noreply.github.com>
2022-06-02 21:51:41 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2C_BANGLOY, MARK KEVIN B._LAB REPORT#3/double (creation).py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data;
+        self.previous = None;
+        self.next = None;
+
+
+class CountList:
+    def __init__(self):
+        self.head = None;
+        self.tail = None;
+
+    def addNode(self, data):
+        newNode = Node(data);
+        if (self.head == None):
+            self.head = self.tail = newNode;
+            self.head.previous = None;
+            self.tail.next = None;
+        else:
+            self.tail.next = newNode;
+            newNode.previous = self.tail;
+            self.tail = newNode;
+            self.tail.next = None;
+
+    def countNodes(self):
+        counter = 0;
+        current = self.head;
+        while (current != None):
+            counter = counter + 1;
+            current = current.next;
+        return counter;
+
+    def display(self):
+        current = self.head;
+        if (self.head == None):
+            print("List is empty");
+            return;
+        print("Nodes of doubly linked list: ");
+        while (current != None):
+            print(current.data),
+            current = current.next;
+
+dList = CountList();
+dList.addNode(10);
+dList.addNode(20);
+dList.addNode(30);
+dList.addNode(40);
+
+dList.display();
+
+print("\nCount of nodes present in the list: " + str(dList.countNodes()));  
